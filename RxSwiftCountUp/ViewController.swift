6bb24454f30a5916e97037aecd8644172237aebb
sauna1137 <subscription.search@gmail.com>
2021-12-21 17:33:05 +0900

    first commit

--- RxSwiftCountUp/ViewController.swift
+++ RxSwiftCountUp/ViewController.swift
@@ -6,14 +6,25 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var countUpButton: UIButton!
+
+    private var count = 0
+    private let disposeBag = DisposeBag()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        countUpButton.rx.tap
+            .subscribe(onNext: {[unowned self] _ in
+                self.count += 1
+                self.countLabel.text = String(self.count)
+            })
+            .disposed(by: disposeBag)
     }
-
-
 }
-
