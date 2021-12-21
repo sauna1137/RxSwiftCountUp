ac8d49c847a5d387e0bf50d458ef605374aefa33
sauna1137 <subscription.search@gmail.com>
2021-12-21 17:35:56 +0900

    コメント追加

--- RxSwiftCountUp/ViewController.swift
+++ RxSwiftCountUp/ViewController.swift
@@ -20,11 +20,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+//「rx.tap」はUIButtonに用意されている、ボタンがタップされたことを通知してくれるObservable
+//「subscribe」は文字通り購読の意味で、対象に変化があった時に呼ばれます。今回はfavButtonがtapされる時
+//「onNext」はイベントが通常通りであるたびに通知します。他にもエラー時に通知するonError、完了時に通知するonCompletedがあります。
+
         countUpButton.rx.tap
             .subscribe(onNext: {[unowned self] _ in
                 self.count += 1
                 self.countLabel.text = String(self.count)
             })
+//「disposed」は購読の解除です。onNextの場合は、イベント後も購読が継続するため、メモリーリークになる恐れがあります。なのでdisposedで開放してあげる必要があります。
             .disposed(by: disposeBag)
     }
 }
